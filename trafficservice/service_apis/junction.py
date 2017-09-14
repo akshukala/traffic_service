a7d06fd9d957571b15d15d8496f61a3f55d2cc77
Ganesh Bodkhe <ganesh@nimbrisk.com>
2017-09-14 13:32:59 +0530

    junction api modified.

--- trafficservice/service_apis/junction.py
+++ trafficservice/service_apis/junction.py
@@ -6,29 +6,70 @@ from flask.globals import request
 from flask_restful import Resource
 from trafficservice.utils.resource import Resource
 from nt_db.nt_db_app.models import *
+from trafficservice.utils.auth import get_user 
 
 class Junctions(Resource):
     
     def get(self):
-        term = request.args.get('term')
-        if term:
-            junctions = Junction.objects.filter(is_active=True, junction_name__istartswith=str(term)
-                ).values_list('junction_name')
+        type = request.args.get('type')
+        if int(type) == 1:
+         return [{
+                    "junction_id":junction.id,
+                    "junction_name":junction.company_name
+                    }
+                    for junction in Junction.objects.filter(city__city_name =request.args.get('city'), is_active=True)]
         else:
-            junctions = []
+            return [{
+                    "junction_id":junction.id,
+                    "junction_name":junction.junction_name,
+                    "hour": junction.hour,
+                    "minute" : junction.minute,
+                    "second": junction.second,
+                    "day": junction.day,
+                    "month":junction.month,
+                    "year":junction.year,
+                    "phase_no":junction.phase_no,
+                    "status":junction.status,
+                    "normal_time":junction.normal_time,
+                    "step_elased_time":junction.step_elased_time,
+                    "cycle_elased_time1":junction.cycle_elased_time1,
+                    "cycle_elased_time2":junction.cycle_elased_time2,
+                    "working_on":junction.working_on,
+                    "mode":junction.mode,
+                    "total_cycle_time1":junction.total_cycle_time1,
+                    "total_cycle_time2":junction.total_cycle_time2
+                    }
+                    for junction in Junction.objects.filter(id=int(request.args.get('id')))]
 
-        return list(set(junctions))
     get.authenticated = False
 
 
     def post(self):
-        import pdb
-        pdb.set_trace()
+        request_data = request.get_json(force=True)
+        print request.form['junction_name']
         try:
-            City.objects.get_or_create(city_name=str(request.form['city_name']))
+            Junction.objects.create(junction_name=str(request.form['junction_name']),
+                                        city = City.objects.get(id=int(request_data['city'])),
+                                        hour = int(request.form['hour']),
+                                        minute = int(request.form['minute']),
+                                        second = int(request.form['second']),
+                                        day = int(request.form['day']),
+                                        month = int(request.form['month']),
+                                        year = int(request.form['year']),
+                                        phase_no = int(request.form['phase_no']),
+                                        status = int(request.form['status']),
+                                        normal_time = int(request.form['normal_time']),
+                                        step_elased_time = int(request.form['step_elased_time']),
+                                        cycle_elased_time1 = int(request.form['cycle_elased_time1']),
+                                        cycle_elased_time2 = int(request.form['cycle_elased_time2']),
+                                        working_on = int(request.form['working_on']),
+                                        total_cycle_time1 = int(request.form['total_cycle_time1']),
+                                        total_cycle_time2 = int(request.form['total_cycle_time2']),
+                                        mode = int(request.form['mode']),
+                                        is_active = True)
             return {"responseCode":"200",
                     "Message":"""Success"""}
         except:
             return {"responseCode":"400",
                     "Message":"""Something went wrong"""}
-    get.authenticated = False
+    post.authenticated = False
