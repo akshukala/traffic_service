107f7e2598079d743e648fab84af5bbd540e8138
Akshay <akshay@nimbrisk.com>
2017-09-19 15:13:16 +0530

    server changes

--- trafficservice/scripts/server.py
+++ trafficservice/scripts/server.py
@@ -1,6 +1,10 @@
+import django
+django.setup()
 import socket
+import requests
 from threading import Thread
 from SocketServer import ThreadingMixIn
+from nt_db.nt_db_app.models import Junction, Junction_data
 
 TCP_IP = socket.gethostname()
 TCP_PORT = 2020
@@ -33,8 +37,14 @@ class ClientThread(Thread):
                         year = file_list[6] + file_list[7]
                         city_name = file_list[38] + file_list[39] + file_list[40] + file_list[41] + file_list[42]
                         junction_name = file_list[33] + file_list[34] + file_list[35] + file_list[36] + file_list[37]
-                        print city_name
-                        print junction_name
+                        Junction_data.objects.create(junction=Junction.objects.get(junction_name=junction_name),
+                            hour=int(file_list[1]), minute=int(file_list[2]), second=int(file_list[3]), day=int(file_list[4]),
+                            month=int(file_list[5]), year=int(year), phase_no=int(file_list[10]), status=str(file_list[11]),
+                            mode=str(file_list[8]), normal_time=int(file_list[17]), step_elased_time=int(file_list[19]),
+                            cycle_elased_time1=int(file_list[20]), cycle_elased_time2=int(file_list[21]),
+                            working_on=str(file_list[22]), total_cycle_time1=int(file_list[23]), total_cycle_time2=int(file_list[24]),
+                            phase1=str(file_list[25]), phase2=str(file_list[26]), phase3=str(file_list[27]), phase4=str(file_list[28]),
+                            phase5=str(file_list[29]), phase6=str(file_list[30]), phase7=str(file_list[31]), phase8=str(file_list[32]))
                     else:
                         print "wrong file"
                 else:
